ab9b9a985e3d04204a79380adc699b974f526c8a
Jean-Baptiste Bouhier <jb.bouhier@gmail.com>
2017-11-11 12:02:18 -0800

    EarthquakeMarker: Improved code style

--- UCSDUnfoldingMaps/src/module4/EarthquakeMarker.java
+++ UCSDUnfoldingMaps/src/module4/EarthquakeMarker.java
@@ -50,9 +50,9 @@ public abstract class EarthquakeMarker extends SimplePointMarker
 		// Add a radius property and then set the properties
 		java.util.HashMap<String, Object> properties = feature.getProperties();
 		float magnitude = Float.parseFloat(properties.get("magnitude").toString());
-		properties.put("radius", 2*magnitude );
+		properties.put("radius", 2 * magnitude );
 		setProperties(properties);
-		this.radius = 1.75f*getMagnitude();
+		this.radius = 1.75f * getMagnitude();
 	}
 	
 
